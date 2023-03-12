2335fd8dbb4f4b7abe0b602314ae399fcd5736cb
Dumindu-Bandara <dumindubandara1999@gmail.com>
2023-03-12 11:10:26 +1100

    Completed Dataloader

---
+++ Models/test.py
@@ -0,0 +1,12 @@
+# import yaml
+
+# with open('config.yaml') as file:
+#     configs = yaml.safe_load(file)
+
+# print(configs['batch_size'])
+
+# from dataset import SampleDataset
+
+# test_dataset = SampleDataset("../Dataset/Netflix_Dataset/Netflix_first_64_1.npy")
+# print(len(test_dataset))
+# print(test_dataset[1])
